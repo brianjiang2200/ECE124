b6cd10e7f1d388a17e1b269f65366aa2588f6010
Brian Jiang <43393631+brianjiang2200@users.noreply.github.com>
2019-05-15 10:48:31 -0400

    Main Commit

---
+++ sample.vhd
@@ -0,0 +1,18 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+LIBRARY work;
+ENTITY ____ IS
+	PORT
+	( -- put your inputs and outputs here
+	   : IN STD_LOGIC;
+	   : OUT STD_LOGIC
+   )
+END ENTITY _________________;
+
+ARCHITECTURE _____ OF  _____  IS
+
+BEGIN
+
+	 ---- enter stuff here
+END ARCHITECTURE _____;
